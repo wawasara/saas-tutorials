2cf7ac2956b39b48a5c9fcbfd1d74d2ff07d2b6f
Hong <wawasara@naver.com>
2019-08-16 10:42:54 +0900

    Fix the bugs for chat

--- server/router.js
+++ server/router.js
@@ -1,5 +1,6 @@
 const AuthenticationController = require("./controllers/authentication"),
   UserController = require("./controllers/user"),
+  ChatController = require("./controllers/chat"),
   express = require("express"),
   passportService = require("./config/passport"),
   passport = require("passport");
@@ -18,6 +19,7 @@ module.exports = function(app) {
   // Initializing route groups
   const apiRoutes = express.Router(),
     userRoutes = express.Router(),
+    chatRoutes = express.Router(),
     authRoutes = express.Router();
 
   //=========================
@@ -64,6 +66,33 @@ module.exports = function(app) {
     (req, res) => {
       res.send({ content: "Admin dashboard is working." });
     }
+  );
+
+  //=========================
+  // Chat Routes
+  //=========================
+
+  // Set chat routes as a subgroup/middleware to apiRoutes
+  apiRoutes.use("/chat", chatRoutes);
+
+  // View messages to and from authenticated user
+  chatRoutes.get("/", requireAuth, ChatController.getConversations);
+
+  // Retrieve single conversation
+  chatRoutes.get(
+    "/:conversationId",
+    requireAuth,
+    ChatController.getConversation
+  );
+
+  // Send reply in conversation
+  chatRoutes.post("/:conversationId", requireAuth, ChatController.sendReply);
+
+  // Start new conversation
+  chatRoutes.post(
+    "/new/:recipient",
+    requireAuth,
+    ChatController.newConversation
   );
 
   // Set url for API group routes
